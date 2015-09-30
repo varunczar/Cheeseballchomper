46466a9bbe7b137a51bb730ee2273fef3f2c43c2
varunczar <varunczar@gmail.com>
2015-09-30 13:30:19 +1000

    First Game

--- desktop/src/com/appswecan/cheeseballchomper/desktop/DesktopLauncher.java
+++ desktop/src/com/appswecan/cheeseballchomper/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.appswecan.cheeseballchomper.CheeseballChomper;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new CheeseballChomper(), config);
 	}
 }
