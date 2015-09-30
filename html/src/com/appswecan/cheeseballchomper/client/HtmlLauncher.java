e6d9f51c5d043a24a62bacaf3517461c338cbc4b
varunczar <varunczar@gmail.com>
2015-09-30 12:00:30 +1000

    Pilot

---
+++ html/src/com/appswecan/cheeseballchomper/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.appswecan.cheeseballchomper.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.appswecan.cheeseballchomper.CheeseballChomper;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CheeseballChomper();
+        }
+}
