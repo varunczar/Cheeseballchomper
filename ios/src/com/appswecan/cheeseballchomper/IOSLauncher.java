86913422fcdc8e7c4a4a27af26af75734060c29b
varunczar <varunczar@gmail.com>
2015-10-08 10:47:16 +1100

    IOS Admob

--- ios/src/com/appswecan/cheeseballchomper/IOSLauncher.java
+++ ios/src/com/appswecan/cheeseballchomper/IOSLauncher.java
@@ -1,18 +1,31 @@
 package com.appswecan.cheeseballchomper;
 
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.coregraphics.CGSize;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.foundation.NSPropertyList;
 import org.robovm.apple.foundation.NSURL;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIViewController;
 import org.robovm.pods.facebook.core.FBSDKAppEvents;
 import org.robovm.pods.facebook.core.FBSDKApplicationDelegate;
+import org.robovm.pods.google.mobileads.GADAdSize;
+import org.robovm.pods.google.mobileads.GADBannerView;
+import org.robovm.pods.google.mobileads.GADRequest;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.appswecan.cheeseballchomper.CheeseballChomper;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+
+    private UIViewController rootViewController;
+    private GADBannerView bannerView;
+
+    private static final String BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
@@ -40,6 +53,62 @@ public class IOSLauncher extends IOSApplication.Delegate {
         // I know kinda weird. But keep it this way for now.
         boolean finished = super.didFinishLaunching(application, launchOptions);
         FBSDKApplicationDelegate.getSharedInstance().didFinishLaunching(application, launchOptions);
+        this.createAd();
         return finished;
     }
+
+    /**
+     * Creates a new admob ad
+     */
+    private void createAd() {
+        rootViewController = UIApplication.getSharedApplication().getKeyWindow().getRootViewController();
+        bannerView = new GADBannerView(GADAdSize.Banner());
+        CGSize screenSize = UIScreen.getMainScreen().getBounds().getSize();
+        CGSize adSize = bannerView.getBounds().getSize();
+
+        //Portrait bottom of screen banner
+
+        double adWidth = adSize.getWidth();
+        double adHeight = adSize.getHeight();
+        double screenHeight = screenSize.getHeight();
+        double screenWidth = screenSize.getWidth();
+        float bannerWidth = (float) screenWidth;
+        float bannerHeight = (float) (bannerWidth / adWidth * adHeight);
+        double adX = (screenWidth / 2) - (adWidth / 2);
+        double adY = screenHeight - bannerHeight;
+
+        //Landscape bottom screen banner
+        /*
+        double adWidth = adSize.getWidth();
+        double adHeight = adSize.getHeight();
+        double screenHeight = screenSize.getHeight();
+        double screenWidth = screenSize.getWidth();
+        float bannerWidth = (float) screenWidth/2;
+        float bannerHeight = (float) ((float) screenHeight/10.0);
+        double adX = (screenWidth / 2) - (adWidth / 2);
+        double adY = 0;
+        */
+
+        bannerView.setFrame(new CGRect(adX, adY, bannerWidth, bannerHeight));
+
+
+        bannerView.setAdUnitID(BANNER_AD_UNIT_ID);
+        rootViewController.getView().addSubview(bannerView);
+
+        bannerView.setRootViewController(rootViewController);
+
+        GADRequest request = new GADRequest();
+
+        bannerView.loadRequest(request);
+
+        this.showAds(true);
+    }
+
+    public void showAds(boolean show) {
+        if (bannerView != null) {
+            bannerView.setHidden(!show);
+        }
+    }
+
+
 }
